e38a598b0fda38409509e55ecec88d43fbd71285
Nidhiisinhaa <nidhiiiisinhaa@gmail.com>
2025-11-19 21:45:03 +0530

    new pages added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from './context/ThemeContext';
+
 // import HomePage from './landingPage/home/HomePage';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -7,7 +9,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
--- frontend/src/landingPage/support/Hero.js
+++ frontend/src/landingPage/support/Hero.js
@@ -1,5 +1,35 @@
+// function Hero() {
+//     return ( <h1>Hero</h1> );
+// }
+
+// export default Hero;
+
+import React from "react";
+
 function Hero() {
-    return ( <h1>Hero</h1> );
+  return (
+    <div
+      style={{
+        width: "100%",
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1520607162513-77705c0f0d4a?auto=format&fit=crop&w=1400&q=80')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        padding: "80px 20px",
+        textAlign: "center",
+        color: "white",
+      }}
+    >
+      <h1 style={{ fontSize: "42px", fontWeight: "700", marginBottom: "10px" }}>
+        CONTACT US
+      </h1>
+
+      <p style={{ fontSize: "18px", maxWidth: "700px", margin: "auto" }}>
+        Need help? You are welcome to contact us anytime. Our team will get in
+        touch with you shortly.
+      </p>
+    </div>
+  );
 }
 
 export default Hero;
